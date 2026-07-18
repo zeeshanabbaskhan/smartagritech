7472937faa2ceeb8f4c0b06cd14a2068523740df
zeeshan-890 <zzabbaskhan830@gmail.com>
2026-07-18 14:18:32 +0500

    Import react-grid-layout styles in app entry

--- web_frontend/src/main.jsx
+++ web_frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'react-grid-layout/css/styles.css'
+import 'react-resizable/css/styles.css'
 import './bones/registry'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
